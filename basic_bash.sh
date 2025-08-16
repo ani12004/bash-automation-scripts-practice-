e3793e412853f1a59b4528403269151a5ff296e5
ANIL GOPAL SUTHAR <124684076+ani12004@users.noreply.github.com>
2025-08-16 21:34:15 +0530

    Create basic_bash.sh
    version v1

---
+++ basic_bash.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+################################
+# Author: Anil
+# Date: 16 jan 2025
+#
+#
+# This file create a director and and also redirects into it.
+################################
+
+# create a director named practce
+mkdir practice
+
+# directs to the practice director
+cd practice
