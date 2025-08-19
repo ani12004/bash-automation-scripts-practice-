11f7c2c48795774f031a39401583d665b21fb182
ANIL SUTHAR <124684076+ani12004@users.noreply.github.com>
2025-08-19 09:21:34 +0530

    Create to_install_ubuntu.sh

---
+++ to_install_ubuntu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+###############################
+# Author : Anil
+# Date : 19 aug 25
+#
+# automates the installation of ubuntu
+###############################
+
+# creates seprate directory or folder for ubuntu
+mkdir ubuntu
+
+# chnages the pwd to ubuntu folder
+cd ubuntu
+
+# now the comman dto install ubuntu 
+sudo apt install ubuntu  # mention the ubuntu version to be intslled
+
+
